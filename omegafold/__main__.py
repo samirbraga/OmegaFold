0baa17fe08e8ac6dbcd846b5935b8f629de086bb
samirbraga <samirchavess@gmail.com>
2026-01-12 16:36:19 +0000

    fix: update script entry point to point to the correct module in omegafold

--- omegafold/__main__.py
+++ omegafold/__main__.py
@@ -138,3 +138,22 @@ def _main(rank, args, inputs):
             del output
             gc.collect()
         xm.master_print("Done!")
+
+
+if __name__ == '__main__':
+    args = pipeline.get_args()
+
+    # create the output directory
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    inputs = pipeline.fasta2inputs(
+        args.input_file,
+        num_pseudo_msa=args.num_pseudo_msa,
+        output_dir=args.output_dir,
+        mask_rate=args.pseudo_msa_mask_rate,
+        num_cycle=args.num_cycle,
+    )
+    inputs = list(inputs)
+    print(f"Total inputs: {len(inputs)}")
+
+    torch_xla.launch(_main, args=(args, inputs))
